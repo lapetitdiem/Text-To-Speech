6702fef9062dc8ba733f1626c802bbdb4012efba
21521953 <21521953@gm.uit.edu.vn>
2024-05-18 21:12:11 +0700

    Input app secret to implement session

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request,send_from_directory, session
 from To_Speech.convert import textToSpeech
+from datetime import datetime
 import os
 app = Flask(__name__)
+app.secret_key = '04468aff0e3acbb6c65b62e4150e227d'
 @app.route('/')
 def index():
     history = session.get('history',[])
@@ -11,9 +13,9 @@ def index():
 def result() :
     textInput = request.form['textInput']
     mp3_path = textToSpeech(textInput)
-    # history = session.get('history',[])
-    # history.append(textInput)
-    # session['history'] = history
+    history = session.get('history',[])
+    history.append(textInput)
+    session['history'] = history
     return render_template('inputResult.html', data =textInput, mp3_filename=mp3_path)
 
 @app.route('/MP3/<filename>')
